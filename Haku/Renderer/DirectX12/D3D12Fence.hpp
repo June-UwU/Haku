0d67557723ba259972f7ef8082be325e083373d5
June <williamreji66@gmail.com>
2022-04-06 11:06:33 -0700

    fence abstraction

---
+++ Haku/Renderer/DirectX12/D3D12Fence.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "macros.hpp"
+#include "hakupch.hpp"
+#include "directx/d3d12.h"
+
+namespace Haku
+{
+namespace Renderer
+{
+class Fence
+{
+public:
+	Fence(D3D12_FENCE_FLAGS flags = D3D12_FENCE_FLAG_NONE);
+	~Fence() noexcept;
+
+	const ID3D12Fence* Get() noexcept;
+	void			   Close() noexcept;
+	void			   Synchronize();
+
+private:
+	void ShutDown() noexcept;
+
+private:
+	ID3D12Fence* m_Fence;
+	HANDLE		 m_FenceEvent;
+};
+} // namespace Renderer
+} // namespace Haku
