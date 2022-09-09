766f96820376caf289ff82f1e52889bb51dd8517
June-UwU <williamreji66@gmail.com>
2022-09-09 09:57:13 +0530

    shader untested

--- Engine/renderer/backends/directx/shader.cpp
+++ Engine/renderer/backends/directx/shader.cpp
@@ -1,1 +1,67 @@
+/*****************************************************************//**
+ * \file   shader.cpp
+ * \brief  shader compilation and implementation file
+ * 
+ * \author June
+ * \date   September 2022
+ *********************************************************************/
+#pragma comment(lib , "D3DCompiler.lib")
+
 #include "shader.hpp"
+#include "core/logger.hpp"
+
+
+i8 compile_shader(const directx_context* context, const char* path, shader_stages stage, directx_shader_module* module)
+{
+	wchar_t* wide_path = nullptr;
+	mbstowcs(wide_path, path, strlen(path));
+
+	const char* target_ptr = nullptr;
+
+	UINT flags = D3DCOMPILE_ENABLE_STRICTNESS;
+#if defined( DEBUG ) || defined( _DEBUG )
+	flags |= D3DCOMPILE_DEBUG;
+#endif
+
+	ID3DBlob* error_blob = nullptr;
+
+	if (nullptr == wide_path)
+	{
+		HLCRIT("path is none");
+		return H_FAIL;
+	}
+
+	HRESULT api_ret_val = D3DCompileFromFile(wide_path,NULL,D3D_COMPILE_STANDARD_FILE_INCLUDE,SHADER_MAIN,
+		SHADER_PROFILE_11_0[stage],flags,0,&module->compiled_shaders[stage],&error_blob
+	);
+
+	if (S_OK != api_ret_val)
+	{
+		HLCRIT("shader compilation failure : %s", (char*)error_blob->GetBufferPointer());
+		return H_FAIL;
+	}
+
+	HLINFO("shader compilation for path : %s ", path);
+	
+	return H_OK;
+}
+
+i8 create_shader_byte_code(const directx_context* context, const char* path, shader_stages stage, directx_shader_module* module)
+{
+	wchar_t* wide_path = nullptr;
+	mbstowcs(wide_path, path, strlen(path));
+
+	if (nullptr == wide_path)
+	{
+		HLCRIT("shader byte code creation failed : %s ", path);
+		return H_FAIL;
+	}
+	HRESULT api_ret_val = D3DReadFileToBlob(wide_path, &module->compiled_shaders[stage]);
+	if (S_OK != api_ret_val)
+	{
+		HLCRIT("shader byte code creation failed : %s ", path);
+		return H_FAIL;
+	}
+
+	return H_OK;
+}
--- Engine/renderer/backends/directx/shader.hpp
+++ Engine/renderer/backends/directx/shader.hpp
@@ -1,5 +1,57 @@
 #pragma once
-
+/*****************************************************************//**
+ * \file   shader.hpp
+ * \brief  shader compilation and creation system
+ * 
+ * \author June
+ * \date   September 2022
+ *********************************************************************/
+#include "defines.hpp"
 #include "directx_types.INL"
+
+// @breif	entry point for every shader is fixed
+constexpr const char* SHADER_MAIN = "main";
+
+/** shader profile under which it will be compiled against  */
+constexpr const char* SHADER_PROFILE_11_0[HK_SHADER_MAX]{
+	"vs_5_0", // vertex shader stage target enumerations
+	"hs_5_0", // hull shader stage target enumerations
+	"ds_5_0", // domain shader stage target enumerations
+	"gs_5_0", // geometry shader stage target enumerations
+	"ps_5_0", // pixel shader stage target enumerations
+	"cs_5_0"  // compute shader stage target enumerations
+};
+
+/** shader profile for 10.0  support */
+constexpr const char* SHADER_PROFILE_10_0[HK_SHADER_MAX]{
+	"vs_4_0", // vertex shader stage enumerations
+	"",		  // hull shader doesn't exist here
+	"",		  // domain shader doesn't exist here
+	"gs_4_0", // geometry shader stage enumerations
+	"ps_4_0", // pixel shader stage enumerations
+	"cs_4_0"  // compute shader stage enumerations
+};
+
+/**
+ * function to compile shaders at runtime.
+ * 
+ * \param context pointer to directx_context 
+ * \param path pointer to the file path
+ * \param stage stage for the directx compile
+ * \param module module to compile and place the shader into
+ * \return H_OK on sucess else H_FAIL
+ */
+i8 compile_shader(const directx_context* context, const char* path, shader_stages stage,directx_shader_module* module);
+
+/**
+ * function to create code byte code blob from a compiled shader.
+ *
+ * \param context pointer to directx_context
+ * \param path pointer to the file path
+ * \param stage stage for the directx compile
+ * \param module module to compile and place the shader into
+ * \return H_OK on sucess else H_FAIL
+ */
+i8 create_shader_byte_code(const directx_context* context, const char* path, shader_stages stage, directx_shader_module* module);
 
 
