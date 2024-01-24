971e4be46dc848c98ae9233a2020bc7ca12fbd60
June <williamreji66@gmail.com>
2024-01-24 23:06:39 +0530

    tiny bug spotted

--- Engine/renderer/renderer.cpp
+++ Engine/renderer/renderer.cpp
@@ -297,6 +297,27 @@ void render() {
 	
 	glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &triangleMVP[0][0]);
 
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glVertexAttribPointer(
+       0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+       3,                  // size
+       GL_FLOAT,           // type
+       GL_FALSE,           // normalized?
+       0,                  // stride
+       (void*)0            // array buffer offset
+    );
+
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	glVertexAttribPointer(
+	    1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+	    3,                                // size
+	    GL_FLOAT,                         // type
+	    GL_FALSE,                         // normalized?
+	    0,                                // stride
+	    (void*)0                          // array buffer offset
+	);
 
     glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
 
