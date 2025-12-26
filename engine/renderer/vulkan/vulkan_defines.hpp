ee171d4861781f145e2b6a58c5299e3f33d70b00
June-UwU <williamreji66@gmail.com>
2025-12-26 18:05:26 +0530

    create the vulkan instance, for the engine
    Change-Id: I676a6f0f8028798828585489123f5e4b39fffc24
    Signed-off-by: June-UwU <williamreji66@gmail.com>

--- engine/renderer/vulkan/vulkan_defines.hpp
+++ engine/renderer/vulkan/vulkan_defines.hpp
@@ -2,6 +2,7 @@
 #include <vulkan/vulkan.hpp>
 #include "../../logger.hpp"
 
-#define VALIDATE_RESULT(result, reason) if(VK_SUCCESS == (result))	\
-    { \
-      FATAL << "Vulkan error: " << << reason << "\n"; std::abort();}
+#define VALIDATE_RESULT(result, reason)                                        \
+  if (VK_SUCCESS != (result)) {                                                \
+    FATAL << "Vulkan error: " << result << "\nreason : " << reason << "\n";    \
+    std::abort();}
